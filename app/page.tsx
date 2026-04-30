97f71b662b849e2806e8fc5ce5f586bf5fda6d37
Emersson7AOR <rodrigoalvarezordinola@gmail.com>
2026-04-29 19:03:56 -0600

    feat: create home page with hero, experience, and about sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/sections/Hero";
+import Experience from "../components/sections/Experience";
 import About from "../components/sections/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Experience />
       <About />
     </main>
   );
